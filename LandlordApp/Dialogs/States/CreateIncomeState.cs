9195922b2be8ae9ab319b54022de93b606a9fd90
Peter.Mulvaney <peter.mulvaney@wolterskluwer.com>
2016-12-14 16:39:04 +0000

    Missed File

--- LandlordApp/Dialogs/States/CreateIncomeState.cs
+++ LandlordApp/Dialogs/States/CreateIncomeState.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
 
 namespace LandlordApp.Dialogs.States {
     [Serializable]
@@ -37,5 +39,8 @@ namespace LandlordApp.Dialogs.States {
             throw new NotImplementedException();
         }
 
+        public string None(IDialogContext context, LuisResult result) {
+            throw new NotImplementedException();
+        }
     }
 }
