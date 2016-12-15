f1427c689e0d5e346328ff5ebd9625b79bf3104a
Glenn.Turner <Glenn.Turner@WLKGTURNER>
2016-12-15 11:15:55 +0000

    save expenses to db

---
+++ LandlordApp.Repositories/ExpenseGateway.cs
@@ -0,0 +1,24 @@
+﻿using LandlordApp.DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandlordApp.Repositories {
+    public class ExpenseGateway {
+        public void CreateExpense(Expense expense) {
+
+            MYOB.DAL.DAL dal = GatewayHelper.GetDal();
+
+            dal.RunSp("CreateExpense",
+                new MYOB.DAL.DalParm("PropertyId", System.Data.SqlDbType.Int, 0, expense.Property.PropertyId),
+                new MYOB.DAL.DalParm("Description", System.Data.SqlDbType.VarChar, 0, expense.Description),
+                new MYOB.DAL.DalParm("Date", System.Data.SqlDbType.DateTime, 0, expense.Date.ToString("s")),
+                new MYOB.DAL.DalParm("Amount", System.Data.SqlDbType.Money, 0, expense.Amount)
+            );
+
+            dal = null;
+        }
+    }
+}
